d45bc2d4a52a48f41f8681316c4088180a0d2d2c
clawrence00 <callumlawrence1@gmail.com>
2023-04-09 16:56:07 +0000

    added division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,7 +44,7 @@ function runGame(gameType) {
   } else if (gameType === "multiply") {
       displayMultiplyQuestion(num1, num2);
   } else if (gameType === "division") {
-      displayDivisionQuestion(num1, num2);
+      displayDivideQuestion(num1, num2);
   } else {
       alert(`Unknown game type: ${gameType}`);
       throw `Unknown game type: ${gameType}. Aborting!`;
@@ -135,7 +135,7 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivideQuestion(operand1, operand2) {
-  document.getElementById('operand1').textContent = operand1;
+  document.getElementById('operand1').textContent = operand1 * operand2;
   document.getElementById('operand2').textContent = operand2;
   document.getElementById('operator').textContent = "/";
 }
